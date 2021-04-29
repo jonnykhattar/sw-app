94bf009c7598549274b92324e04a7bc2097377d5
Jonathan Khattar <jonathan.khattar91@gmail.com>
2021-04-29 23:14:50 +1000

    moved static data to Data file

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { baseDef, requiredDef, guildbuff, determinationSetsValues, towerLevels } from './Data.js';
 
 export class Calculator extends Component {
 
@@ -24,33 +25,24 @@ export class Calculator extends Component {
 
     }
 
-    baseDef = 736;
-    requiredDef = 3301;
-    towerLevels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    determinationSetsValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    guildbuff = {
-        0: 0,
-        3: 0.03,
-        5: 0.05
-    }
     remainingDef;
 
     updateRemainingDef = () => {
-        this.remainingDef = Math.round((this.requiredDef - this.baseDef) - (0.08 * this.state.numberOfDeterminationSets * this.baseDef) - ((this.state.defGloryTowerLevel * 0.02) * this.baseDef)
-            - (this.baseDef * this.state.guildBuffValue));
+        this.remainingDef = Math.round((requiredDef - baseDef) - (0.08 * this.state.numberOfDeterminationSets * baseDef) - (this.state.defGloryTowerLevel * baseDef)
+            - (baseDef * this.state.guildBuffValue));
         this.forceUpdate();
     }
 
     render() {
         return (
             <div>
-                <img src="https://summonerswarskyarena.info/wp-content/uploads/2019/06/unit_icon_0051_0_3.png"/>
+                <img src="https://summonerswarskyarena.info/wp-content/uploads/2019/06/unit_icon_0051_0_3.png" alt="Icaru icon"/>
                 <h1>Defence Required: {this.remainingDef}</h1>
                 <form>
                     <label>Defence Glory Tower Level: </label>
                     <select value={this.state.defGloryTowerLevel} onChange={this.handleChange} name='defGloryTowerLevel'>
                         {
-                            this.towerLevels.map((towerLevel) => {
+                            towerLevels.map((towerLevel) => {
                                 return <option value={towerLevel} key={towerLevel}>{towerLevel}</option>
                             }
                             )
@@ -60,8 +52,8 @@ export class Calculator extends Component {
                     <label>Guild Buff Level: </label>
                     <select value={this.state.guildBuffValue} onChange={this.handleChange} name='guildBuffValue'>
                         {
-                            Object.keys(this.guildbuff).map((key) => {
-                                return <option value={this.guildbuff[key]} key={key} >{key}%</option>
+                            Object.keys(guildbuff).map((key) => {
+                                return <option value={guildbuff[key]} key={key} >{key}%</option>
                             })
                         }
                     </select>
@@ -69,7 +61,7 @@ export class Calculator extends Component {
                     <label>Number of Determination Sets: </label>
                     <select value={this.state.numberOfDeterminationSets} onChange={this.handleChange} name='numberOfDeterminationSets'>
                         {
-                            this.determinationSetsValues.map((setNum) => {
+                            determinationSetsValues.map((setNum) => {
                                 return <option value={setNum} key={setNum}>{setNum}</option>
                             }
                             )
---
+++ src/Data.js
@@ -0,0 +1,9 @@
+export const baseDef = 736;
+export const requiredDef = 3301;
+export const towerLevels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+export const determinationSetsValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+export const guildbuff = {
+    0: 0,
+    3: 0.03,
+    5: 0.05
+}
